09a96ae0666658c410aeceb0b47f19b2f8bbe2ce
jwwallin <jussi.w.wallin@gmail.com>
2016-12-12 21:50:25 +0200

    Created sevensegment display library file

---
+++ BinarieLibrary/SevenSegment.h
@@ -0,0 +1,19 @@
+/*
+
+Title: 					BinarieLibrary - SevenSegment
+Original creator: 		Jussi Wallin
+Original release URL: 	https://github.com/Binarieshield/BinarieLibrary
+Original release date:	12/12/2016 (dd/mm/yyyy)
+License:
+This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
+
+*/
+
+
+
+#ifndef BinarieSevenSeg_h
+#define BinarieSevenSeg_h
+
+
+
+#endif
