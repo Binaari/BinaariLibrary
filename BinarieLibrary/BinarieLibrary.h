79e3de24c02316171c1b89d790562d20d5a95133
jwwallin <jussi.w.wallin@gmail.com>
2016-12-12 21:29:06 +0200

    Added main library file and example of library

---
+++ BinarieLibrary/BinarieLibrary.h
@@ -0,0 +1,21 @@
+/*
+
+Title: 					BinarieLibrary
+Original creator: 		Jussi Wallin
+Original release URL: 	https://github.com/Binarieshield/BinarieLibrary
+Original release date:	12/12/2016 (dd/mm/yyyy)
+License:
+This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
+
+*/
+
+
+
+#ifndef BinarieLib_h
+#define BinarieLib_h
+
+class BinarieLibrary {
+
+}
+
+#endif
