037a86cdfb263fd69cdb0aa12afcad12cc1608db
jwwallin <jussi.w.wallin@gmail.com>
2016-12-30 22:55:33 +0200

    Started building latest lib

--- BinaariLibrary/RotationalEncoder.h
+++ BinaariLibrary/RotationalEncoder.h
@@ -11,14 +11,41 @@ This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 Inte
 
 
 
-#ifndef BinaariSevenSeg_h
-#define BinaariSevenSeg_h
+#ifndef BinaariEncoder_h
+#define BinaariEncoder_h
+
+#include "Arduino.h"
 
 class RotationalEncoder {
 
-    void initiate() {
+    public:
+
+    int a;
+    int b;
+    int button;
+
+    private:
+        
+    volatile unsigned int encoderPos = 0;
+    unsigned int lastReportedPos = 1;
+    volatile boolean turn = false;
+
+
+    void initiate(int A = 3, int B = 2, int btn = 4) {
+        a = A;
+        b = B;
+        button = btn;
+
+        pinMode(A, INPUT_PULLUP);
+        pinMode(B, INPUT_PULLUP);
+        pinMode(button, INPUT_PULLUP);
+
+        attachInterrupt(digitalPinToInterrupt(A), doInterruptA, CHANGE);
+        attachInterrupt(digitalPinToInterrupt(B), doInterruptB, CHANGE);
 
     }
+
+    
 };
 
 #endif
